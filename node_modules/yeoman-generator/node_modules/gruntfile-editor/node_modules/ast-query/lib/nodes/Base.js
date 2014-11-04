7d0cb57bd7db6bf0ba0afef21f751d9ad4a3e4c4
Benjamin CHENEBAULT <benjamin.chenebault@gmail.com>
2014-11-04 12:31:25 +0100

    Suppression des module node

--- node_modules/yeoman-generator/node_modules/gruntfile-editor/node_modules/ast-query/lib/nodes/Base.js
+++
@@ -1,8 +0,0 @@
-var Base = module.exports = function (nodes) {
-  this.nodes = Array.isArray(nodes) ? nodes : [nodes];
-  this.length = this.nodes.length;
-  this.initialize.apply(this, arguments);
-};
-
-Base.prototype.initialize = function () {};
-Base.extend = require('class-extend').extend;
